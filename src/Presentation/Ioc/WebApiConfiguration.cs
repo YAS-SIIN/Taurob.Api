97d99af40fe20572de8961c51e9a8f43f303cfdd
Yasin Asadnezhad <yasin.asadnezhad@msn.com>
2024-08-01 14:40:38 +0330

    Added register class

---
+++ src/Presentation/Ioc/WebApiConfiguration.cs
@@ -0,0 +1,25 @@
+﻿
+using Mc2.CrudTest.Infra.Data.Context;
+using Mc2.CrudTest.Infra.Data.UnitofWork;
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.InMemory;
+
+using Taurob.Api.Domain.Interfaces.UnitOfWork;
+
+namespace Taurob.Api.Presentation.Ioc;
+
+public static class WebApiConfiguration
+{
+    public static void Register(this IServiceCollection services)
+    {
+        //Register in memory database
+        services.AddDbContext<TaurobDBContext>(options => options.UseInMemoryDatabase("TaurobDBContext"));
+
+        //Register unit of work service
+        services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+    }
+
+}
--- src/Presentation/WebApi/Program.cs
+++ src/Presentation/WebApi/Program.cs
@@ -1,3 +1,5 @@
+using Taurob.Api.Presentation.Ioc;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.Register();
 
 var app = builder.Build();
 
