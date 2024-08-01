dbc7c95ea26ed05b2ca32106b61b5f8d5ad815e5
Yasin Asadnezhad <yasin.asadnezhad@msn.com>
2024-08-01 20:57:47 +0330

    Added exeption and mapper

---
+++ src/Presentation/Taurob.Api.Presentation.Shared/Exceptions/ForbiddenAccessException.cs
@@ -0,0 +1,6 @@
+﻿namespace Taurob.Api.Presentation.Shared.Exceptions;
+
+public class ForbiddenAccessException : Exception
+{
+    public ForbiddenAccessException() : base() { }
+}
---
+++ src/Presentation/Taurob.Api.Presentation.Shared/Mapper/Mapper.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+
+namespace Taurob.Api.Presentation.Shared.Mapper;
+
+public static class Mapper<TDestination, TOrigin>
+{
+    public static TDestination MappClasses(TOrigin command)
+    {
+        var config = new MapperConfiguration(cfg => cfg.CreateMap<TOrigin, TDestination>());
+
+        var mapper = config.CreateMapper();
+        return mapper.Map<TDestination>(command);
+    }    
+}
