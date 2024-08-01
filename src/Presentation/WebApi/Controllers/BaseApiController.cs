43f0e0d9cd20bb81d18f446b6c341d14fd3697e1
Yasin Asadnezhad <yasin.asadnezhad@msn.com>
2024-08-01 21:25:33 +0330

    Coded and impelemented mission api.

---
+++ src/Presentation/WebApi/Controllers/BaseApiController.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+using Taurob.Api.Domain.DTOs.Exceptions;
+
+namespace Taurob.Api.Presentation.WebApi.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public abstract class BaseApiController : ControllerBase
+{
+    private IMediator _mediator;      
+    protected IMediator Mediator => _mediator ??= HttpContext.RequestServices.GetService<IMediator>();
+
+    public IActionResult OkData<TData>(ResultDto<TData> response)
+    {
+        return new ObjectResult(response);
+    }
+}
---
+++ src/Presentation/WebApi/Controllers/MissionController.cs
@@ -0,0 +1,74 @@
+﻿ 
+using MediatR;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+using Taurob.Api.Core.Commands.Mission;
+using Taurob.Api.Core.Queries.Mission;
+
+namespace Taurob.Api.Presentation.WebApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class MissionController : BaseApiController
+{
+
+    /// <summary>
+    /// Get all mission row.
+    /// </summary>
+    /// <returns>Result</returns>
+    [HttpGet]
+    public async Task<IActionResult> GetAll()
+    {
+        var query = new GetAllMissionQuery();
+        return OkData(await Mediator.Send(query));
+    }
+
+   /// <summary>
+   /// Return list of mission
+   /// </summary>
+   /// <param name="query"></param>
+   /// <param name="cancellation"></param>
+   /// <returns></returns>
+    [HttpGet("{Id}")]
+    public async Task<IActionResult> Get([FromRoute] GetMissionQuery query, CancellationToken cancellation)
+    { 
+        return OkData(await Mediator.Send(query, cancellation));
+    }
+
+    /// <summary>
+    /// Creates a new mission.
+    /// </summary>
+    /// <param name="command"></param>
+    /// <returns>Result</returns>
+    [HttpPost]
+    public async Task<IActionResult> Create(CreateMissionCommand command, CancellationToken cancellation)
+    {
+        return OkData(await Mediator.Send(command, cancellation));
+    }
+
+    /// <summary>
+    /// Update a mission.
+    /// </summary>
+    /// <param name="command"></param>
+    /// <returns>Result</returns>
+    [HttpPut]
+    public async Task<IActionResult> Update(UpdateMissionCommand command, CancellationToken cancellation)
+    {
+        return OkData(await Mediator.Send(command, cancellation));
+    }
+
+    /// <summary>
+    /// Delete a mission.
+    /// </summary>
+    /// <param name="command"></param>
+    /// <param name="cancellation"></param>
+    /// <returns></returns>
+    [HttpDelete("{Id}")]
+    public async Task<IActionResult> Delete([FromRoute] DeleteMissionCommand command, CancellationToken cancellation)
+    { 
+        return OkData(await Mediator.Send(command, cancellation));
+    }
+
+}
