d6276f02c86058c368b3c16ef773fccad938869c
Yasin Asadnezhad <yasin.asadnezhad@msn.com>
2024-08-02 04:20:01 +0330

    Coded unit test for get robot data.

---
+++ tests/Taurob.Api.UnitTest/Handlers/Robot/Query/GetAllRobotQuery_Test.cs
@@ -0,0 +1,30 @@
+﻿
+using FluentValidation;
+
+using Taurob.Api.Application.Application.UseCases.Robots.Queries;
+using Taurob.Api.Core.Queries.Robot;
+using Taurob.Api.UnitTest;
+namespace Mc2.CrudTest.UnitTest.Handlers.Robot.Query;
+
+public class GetAllRobotQuery_Test
+{
+    private readonly GetAllRobotQueryHandler _getAllRobotQueryHandler;
+    public GetAllRobotQuery_Test()
+    {
+
+        TestTools.Initialize();
+        _getAllRobotQueryHandler = new GetAllRobotQueryHandler(TestTools._dbContext!);
+
+    }
+
+    [Fact]
+    public async Task GetAllRobot_ShouldBeSucceeded()
+    { 
+        var requestData = new GetAllRobotQuery();
+        var responseData = await _getAllRobotQueryHandler.Handle(requestData, CancellationToken.None);
+
+        Assert.NotNull(responseData.Data);
+        Assert.NotEqual(0, responseData.Data.Count);
+    }
+     
+}
---
+++ tests/Taurob.Api.UnitTest/Handlers/Robot/Query/GetRobotQuery_Test.cs
@@ -0,0 +1,42 @@
+﻿
+using Taurob.Api.Application.Application.UseCases.Robots.Queries;
+using Taurob.Api.Core.Queries.Robot;
+using Taurob.Api.Domain.DTOs.Exceptions;
+using Taurob.Api.UnitTest;
+
+namespace Mc2.CrudTest.UnitTest.Handlers.Robot.Query;
+
+public class GetRobotQuery_Test
+{
+    private readonly GetRobotQueryHandler _getRobotQueryHandler;
+    public GetRobotQuery_Test()
+    {
+
+        TestTools.Initialize();
+        _getRobotQueryHandler = new GetRobotQueryHandler(TestTools._dbContext!);
+
+    }
+
+    [Theory]
+    [InlineData(1)]
+    public async Task GetRobot_ShouldBeSucceeded(int id)
+    { 
+        var requestData = new GetRobotQuery { Id = id };
+        var responseData = await _getRobotQueryHandler.Handle(requestData, CancellationToken.None);
+
+        Assert.NotNull(responseData.Data);
+        TestTools._dbContext?.Dispose();
+    }
+
+    [Theory]
+    [InlineData(999)]
+    public async Task GetRobot_ShouldBeFailed(int id)
+    {
+
+        var requestData = new GetRobotQuery { Id = id };
+
+        await Assert.ThrowsAsync<ErrorException>(async () => await _getRobotQueryHandler.Handle(requestData, CancellationToken.None));
+        TestTools._dbContext?.Dispose();
+    }
+
+}
