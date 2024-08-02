a536bf781b0b6dd7896e66cb87bbc7e630075cbc
Yasin Asadnezhad <yasin.asadnezhad@msn.com>
2024-08-02 04:11:58 +0330

    Coded unit test for Get data handlers.

---
+++ tests/Taurob.Api.UnitTest/Handlers/Mission/Query/GetAllMissionQuery_Test.cs
@@ -0,0 +1,30 @@
+﻿
+using FluentValidation;
+
+using Taurob.Api.Application.Application.UseCases.Missions.Queries;
+using Taurob.Api.Core.Queries.Mission;
+using Taurob.Api.UnitTest;
+namespace Mc2.CrudTest.UnitTest.Handlers.Mission.Query;
+
+public class GetAllMissionQuery_Test
+{
+    private readonly GetAllMissionQueryHandler _getAllMissionQueryHandler;
+    public GetAllMissionQuery_Test()
+    {
+
+        TestTools.Initialize();
+        _getAllMissionQueryHandler = new GetAllMissionQueryHandler(TestTools._dbContext!);
+
+    }
+
+    [Fact]
+    public async Task GetAllMission_ShouldBeSucceeded()
+    { 
+        var requestData = new GetAllMissionQuery();
+        var responseData = await _getAllMissionQueryHandler.Handle(requestData, CancellationToken.None);
+
+        Assert.NotNull(responseData.Data);
+        Assert.NotEqual(0, responseData.Data.Count);
+    }
+     
+}
---
+++ tests/Taurob.Api.UnitTest/Handlers/Mission/Query/GetMissionQuery_Test.cs
@@ -0,0 +1,42 @@
+﻿
+using Taurob.Api.Application.Application.UseCases.Missions.Queries;
+using Taurob.Api.Core.Queries.Mission;
+using Taurob.Api.Domain.DTOs.Exceptions;
+using Taurob.Api.UnitTest;
+
+namespace Mc2.CrudTest.UnitTest.Handlers.Mission.Query;
+
+public class GetMissionQuery_Test
+{
+    private readonly GetMissionQueryHandler _getMissionQueryHandler;
+    public GetMissionQuery_Test()
+    {
+
+        TestTools.Initialize();
+        _getMissionQueryHandler = new GetMissionQueryHandler(TestTools._dbContext!);
+
+    }
+
+    [Theory]
+    [InlineData(1)]
+    public async Task GetMission_ShouldBeSucceeded(int id)
+    { 
+        var requestData = new GetMissionQuery { Id = id };
+        var responseData = await _getMissionQueryHandler.Handle(requestData, CancellationToken.None);
+
+        Assert.NotNull(responseData.Data);
+        TestTools._dbContext?.Dispose();
+    }
+
+    [Theory]
+    [InlineData(999)]
+    public async Task GetMission_ShouldBeFailed(int id)
+    {
+
+        var requestData = new GetMissionQuery { Id = id };
+
+        await Assert.ThrowsAsync<ErrorException>(async () => await _getMissionQueryHandler.Handle(requestData, CancellationToken.None));
+        TestTools._dbContext?.Dispose();
+    }
+
+}
